82ca3a3dbcd456f44ce9391a6a0740aba5d0ab4d
Elliot Sayes <elliot@sayes.cloud>
2024-05-26 21:55:56 +0900

    Partially working AWC+AO demo Working: - ArConnect - Browser Wallet Broken: - Arweave.app (disconnects upon closing the prompt) - Othent (CORS error)

--- src/features/ao/components/AoDemo.tsx
+++ src/features/ao/components/AoDemo.tsx
@@ -1,7 +1,28 @@
+import { connect, createDataItemSigner } from "@permaweb/aoconnect";
 import { useApi } from "arweave-wallet-kit";
+
+type UserNew = {
+  name: string;
+  avatar: string;
+  status: string;
+  currentWorldId: string;
+  following: {
+    [address: string]: boolean;
+  };
+}
+
+const userNew: UserNew = {  
+  name: "Elliot",
+  avatar: "a123",
+  status: "s123",
+  currentWorldId: "w123",
+  following: {},
+}
 
 export function AoDemo() {
   const api = useApi();
+
+  const aoClient = connect();
 
   return (
     <div>
@@ -9,6 +30,25 @@ export function AoDemo() {
       <p>
         {api ? "Has API" : "No API"}
       </p>
+      <button onClick={async () => {
+        if (api) {
+          // const compatibleApi = {
+          //   ...api,
+          //   signDataItem: api.sign,
+          // }
+          // const signer = createDataItemSigner(compatibleApi);
+          const signer = createDataItemSigner(window.arweaveWallet);
+          const res = await aoClient.message({
+            process: "PHHOfjFPSbn7TQT1Z-dB4ndyIyppHV5haMPCg8dIccs",
+            data: JSON.stringify(userNew),
+            tags: [{ name: "Action", value: "Register" }],
+            signer,
+          });
+          console.log(res);
+        }
+      }}>
+        Sign
+      </button>
     </div>
   )
 }
