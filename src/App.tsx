56d1e93c6e6d0f63b2ebe3f29e2e9513951e2d28
Elliot Sayes <elliot@sayes.cloud>
2024-05-26 20:57:16 +0900

    Refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import { useState } from 'react'
+import { Home } from "./Home";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Home />
   )
 }
-
-export default App
