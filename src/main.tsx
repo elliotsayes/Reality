21dff8d66d68a4208411c522fca3bcf1d2731e62
Elliot Sayes <elliot@sayes.cloud>
2024-05-28 10:14:35 +0900

    Use hash history (permaweb compat)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { RouterProvider, createHashHistory, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
-const router = createRouter({ routeTree })
+const hashHistory = createHashHistory()
+
+const router = createRouter({ routeTree, history: hashHistory })
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router
