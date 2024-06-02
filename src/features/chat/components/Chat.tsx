63a5293547512271a06eb0c266b852f34b1d023d
Elliot Sayes <elliot@sayes.cloud>
2024-06-02 15:53:59 +0800

    Example Chat component

--- src/features/chat/components/Chat.tsx
+++ src/features/chat/components/Chat.tsx
@@ -2,6 +2,7 @@ import { ArweaveId } from "@/features/arweave/lib/model";
 import { ChatClient } from "../contract/chatClient";
 import { Button } from "@/components/ui/button";
 import { truncateAddress } from "@/features/arweave/lib/utils";
+import { useQuery } from "@tanstack/react-query";
 
 interface ChatProps {
   userAddress: ArweaveId;
@@ -14,6 +15,13 @@ export function Chat({
   chatClient,
   onUserMessageSent,
 }: ChatProps) {
+  const history = useQuery({
+    queryKey: ["chat", chatClient?.aoContractClient.processId, "history"],
+    queryFn: async () => {
+      return await chatClient!.readHistory();
+    },
+    enabled: chatClient?.aoContractClient.processId !== undefined,
+  })
 
   return (
     <div>
@@ -27,6 +35,15 @@ export function Chat({
       >
         Fire `onUserMessageSent`
       </Button>
+      <ul>
+        {
+          history.isSuccess
+            ? Object.entries(history.data).map(([key, value]) => (
+              <li key={key}>{truncateAddress(value.Author)}: {value.Content}</li>
+            ))
+            : <li>Loading...</li>
+        }
+      </ul>
     </div>
   )
 }
