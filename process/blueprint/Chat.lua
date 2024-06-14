e471bdd8cd746c4069a1e944828a1d0b6d58a68a
Elliot Sayes <elliot@sayes.cloud>
2024-06-14 11:58:16 +0200

    Implement Chat with sqlite

--- process/blueprint/Chat.lua
+++ process/blueprint/Chat.lua
@@ -1,33 +1,158 @@
 local json = require("json")
+local sqlite3 = require('lsqlite3')
 
-ChatHistory = ChatHistory or {
-  -- ['msgId'] = {
-  --   Timestamp = 0,
-  --   Author = 'SomeTxId',
-  --   Content = 'Hello World!',
-  -- }
-}
+ChatDb = ChatDb or sqlite3.open_memory()
+ChatDbAdmin = ChatDbAdmin or require('DbAdmin').new(ChatDb)
+
+--#region Initialization
+
+SQLITE_TABLE_CHAT_MESSAAGES = [[
+  CREATE TABLE IF NOT EXISTS Messages (
+    Id TEXT PRIMARY KEY,
+    Timestamp INTEGER,
+    AuthorId TEXT,
+    AuthorName TEXT,
+    Content TEXT
+  );
+]]
+
+function InitDb()
+  ChatDb:exec(SQLITE_TABLE_CHAT_MESSAAGES)
+end
+
+Initialized = Initialized or false
+if (not Initialized) then
+  InitDb()
+  Initialized = true
+end
+
+--#endregion
+
+function ValidateAuthorName(authorName)
+  if (authorName == nil) then
+    return false
+  end
+  if (string.len(authorName) < 1) then
+    return false
+  end
+  if (string.len(authorName) > 20) then
+    return false
+  end
+  if (string.match(authorName, "[^a-zA-Z0-9\\ _-]")) then
+    return false
+  end
+
+  return true
+end
+
+function ValidateContent(content)
+  if (content == nil) then
+    return false
+  end
+  if (string.len(content) < 1) then
+    return false
+  end
+  if (string.len(content) > 100) then
+    return false
+  end
+  -- Note: Input sanitization is not performed here, but by the bind function
+
+  return true
+end
 
 Handlers.add(
   "ChatMessage",
   Handlers.utils.hasMatchingTag("Action", "ChatMessage"),
   function(msg)
-    print("ChatMessage")
-    local entry = {
-      Timestamp = msg.Timestamp,
-      Author = msg.From,
-      Content = msg.Data,
-    }
-    ChatHistory[msg.Id] = entry
+    -- print("ChatMessage")
+    local messageId = msg.Id
+    local timestamp = msg.Timestamp
+    local authorId = msg.From
+    local authorName = msg.Tags['Author-Name']
+    local content = msg.Data
+
+    -- Validate AuthorName
+    if (not ValidateAuthorName(authorName)) then
+      return print("Invalid Author Name")
+    end
+
+    -- Validate Content
+    if (not ValidateContent(content)) then
+      return print("Invalid Content")
+    end
+
+    -- Save message
+    local stmt = ChatDb:prepare([[
+      INSERT INTO Messages (Id, Timestamp, AuthorId, AuthorName, Content)
+      VALUES (?, ?, ?, ?, ?)
+    ]])
+    stmt:bind_values(messageId, timestamp, authorId, authorName, content)
+    stmt:step()
+    stmt:finalize()
+
+    return print("Message saved")
   end
 )
+
+function ValidateTimestamp(testTimestamp)
+  if (testTimestamp == nil) then
+    -- Allow nil timestamps
+    return true
+  end
+  if (testTimestamp < 0) then
+    return false
+  end
+
+  return true
+end
 
 Handlers.add(
   "ChatHistory",
   Handlers.utils.hasMatchingTag("Action", "ChatHistory"),
   function(msg)
-    print("ChatHistory")
-    Handlers.utils.reply(json.encode(ChatHistory))(msg)
+    -- print("ChatHistory")
+    local timestampStart = tonumber(msg.Tags['Timestamp-Start'])
+    local timestampEnd = tonumber(msg.Tags['Timestamp-End'])
+
+    -- Validate Individual Timestamps
+    if (not ValidateTimestamp(timestampStart)) then
+      return print("Invalid Timestamp Start")
+    end
+    if (not ValidateTimestamp(timestampEnd)) then
+      return print("Invalid Timestamp End")
+    end
+
+    -- Validate Range
+    if (timestampStart ~= nil
+          and timestampEnd ~= nil
+          and timestampStart > timestampEnd) then
+      return print("Invalid Timestamp Range")
+    end
+
+    -- Query messages
+    -- Note that timestamps are inclusive and may be nil
+    local stmt = ChatDb:prepare([[
+      SELECT * FROM Messages
+      WHERE (Timestamp >= ? OR ? IS NULL)
+      AND (Timestamp <= ? OR ? IS NULL)
+    ]])
+    stmt:bind_values(timestampStart, timestampStart, timestampEnd, timestampEnd)
+
+    local messages = {}
+    for row in stmt:nrows() do
+      table.insert(messages, {
+        Id = row.Id,
+        Timestamp = row.Timestamp,
+        AuthorId = row.AuthorId,
+        AuthorName = row.AuthorName,
+        Content = row.Content
+      })
+    end
+
+    stmt:finalize()
+
+    -- Reply with messages
+    Handlers.utils.reply(json.encode(messages))(msg)
   end
 )
 
--- process/blueprint/Chat.test.js
+++ process/blueprint/Chat.test.js
@@ -23,3 +23,198 @@ test('load source', async () => {
 
   assert.equal(result.Output.data.output, "Loaded Chat Protocol")
 })
+
+test('ChatMessage no author', async () => {
+  const result = await Send({
+    From: "Some hacker ID",
+    Action: "ChatMessage",
+    Data: "Hello, World!",
+  });
+
+  assert.equal(result.Output.data, "Invalid Author Name")
+});
+
+test('ChatMessage bad author', async () => {
+  const result = await Send({
+    From: "Some hacker ID",
+    Action: "ChatMessage",
+    ['Author-Name']: ";SomeHacker",
+    Data: "Hello, World!",
+  });
+
+  assert.equal(result.Output.data, "Invalid Author Name")
+});
+
+test('ChatMessage bad message', async () => {
+  const result = await Send({
+    From: "Some hacker ID",
+    Action: "ChatMessage",
+    ['Author-Name']: "--Some_-Hacker09",
+    Data: "",
+  });
+
+  assert.equal(result.Output.data, "Invalid Content")
+});
+
+test('ChatMessage valid', async () => {
+  const result = await Send({
+    From: "Some hacker ID",
+    Action: "ChatMessage",
+    ['Author-Name']: "--Some_-Hacker09",
+    Data: "Hello, World!",
+  });
+
+  assert.equal(result.Output.data, "Message saved")
+
+  const countRes = await Send({
+    Action: "Eval",
+    Data: `ChatDbAdmin:count('Messages')`
+  })
+  assert.equal(countRes.Output.data.output, "1")
+  
+  const queryRes = await Send({
+    Action: "Eval",
+    Data: `require('json').encode(ChatDbAdmin:exec('SELECT * FROM Messages')[1])`
+  })
+  assert.deepEqual(JSON.parse(queryRes.Output.data.output), {
+    Id: '1234',
+    Timestamp: 10003,
+    AuthorId: 'Some hacker ID',
+    AuthorName: '--Some_-Hacker09',
+    Content: 'Hello, World!',
+  })
+});
+
+test('ChatHistory no timestamps', async () => {
+  const result = await Send({
+    Action: "ChatHistory",
+  });
+
+  const reply = result.Messages[0]
+  const messages = JSON.parse(reply.Data)
+  assert.equal(messages.length, 1)
+  assert.deepEqual(messages[0], {
+    Content: 'Hello, World!',
+    AuthorName: '--Some_-Hacker09',
+    Id: '1234',
+    Timestamp: 10003,
+    AuthorId: 'Some hacker ID',
+  })
+});
+
+test('ChatMessage high valid', async () => {
+  const result = await Send({
+    Id: '5678',
+    Timestamp: 20000,
+    From: "Some hacker ID",
+    Action: "ChatMessage",
+    ['Author-Name']: "--Some_-Hacker09",
+    Data: "Hello, World2!",
+  });
+
+  assert.equal(result.Output.data, "Message saved")
+
+  const countRes = await Send({
+    Action: "Eval",
+    Data: `ChatDbAdmin:count('Messages')`
+  })
+  assert.equal(countRes.Output.data.output, "2")
+  
+  const queryRes = await Send({
+    Action: "Eval",
+    Data: `require('json').encode(ChatDbAdmin:exec('SELECT * FROM Messages')[2])`
+  })
+  assert.deepEqual(JSON.parse(queryRes.Output.data.output), {
+    Id: '5678',
+    Timestamp: 20000,
+    AuthorId: 'Some hacker ID',
+    AuthorName: '--Some_-Hacker09',
+    Content: 'Hello, World2!',
+  })
+});
+
+test('ChatMessage mid valid', async () => {
+  const result = await Send({
+    Id: '9000',
+    Timestamp: 15000,
+    From: "Some hacker ID",
+    Action: "ChatMessage",
+    ['Author-Name']: "--Some_-Hacker09",
+    Data: "Hello, World3!",
+  });
+
+  assert.equal(result.Output.data, "Message saved")
+
+  const countRes = await Send({
+    Action: "Eval",
+    Data: `ChatDbAdmin:count('Messages')`
+  })
+  assert.equal(countRes.Output.data.output, "3")
+  
+  const queryRes = await Send({
+    Action: "Eval",
+    Data: `require('json').encode(ChatDbAdmin:exec('SELECT * FROM Messages')[3])`
+  })
+  assert.deepEqual(JSON.parse(queryRes.Output.data.output), {
+    Id: '9000',
+    Timestamp: 15000,
+    AuthorId: 'Some hacker ID',
+    AuthorName: '--Some_-Hacker09',
+    Content: 'Hello, World3!',
+  })
+});
+
+test('ChatHistory with Timestamp-Start', async () => {
+  const result = await Send({
+    Action: "ChatHistory",
+    ['Timestamp-Start']: 19999,
+  });
+
+  const reply = result.Messages[0]
+  const messages = JSON.parse(reply.Data)
+  assert.equal(messages.length, 1)
+  assert.deepEqual(messages[0], {
+    Content: 'Hello, World2!',
+    AuthorName: '--Some_-Hacker09',
+    Id: '5678',
+    Timestamp: 20000,
+    AuthorId: 'Some hacker ID',
+  })
+});
+
+test('ChatHistory with Timestamp-End', async () => {
+  const result = await Send({
+    Action: "ChatHistory",
+    ['Timestamp-End']: 10004,
+  });
+
+  const reply = result.Messages[0];
+  const messages = JSON.parse(reply.Data);
+  assert.equal(messages.length, 1);
+  assert.deepEqual(messages[0], {
+    Content: 'Hello, World!',
+    AuthorName: '--Some_-Hacker09',
+    Id: '1234',
+    Timestamp: 10003,
+    AuthorId: 'Some hacker ID',
+  });
+});
+
+test('ChatHistory with Timestamp-Start and Timestamp-End', async () => {
+  const result = await Send({
+    Action: "ChatHistory",
+    ['Timestamp-Start']: 10004,
+    ['Timestamp-End']: 19999,
+  });
+
+  const reply = result.Messages[0];
+  const messages = JSON.parse(reply.Data);
+  assert.equal(messages.length, 1);
+  assert.deepEqual(messages[0], {
+    Content: 'Hello, World3!',
+    AuthorName: '--Some_-Hacker09',
+    Id: '9000',
+    Timestamp: 15000,
+    AuthorId: 'Some hacker ID',
+  });
+});
