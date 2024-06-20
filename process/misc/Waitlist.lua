673f862876e47fe87ff7ecff78c11fea3d27e944
Elliot Sayes <elliot@sayes.cloud>
2024-06-20 12:27:04 +0200

    Waitlist contract

---
+++ process/misc/Waitlist.lua
@@ -0,0 +1,324 @@
+local json = require("json")
+local sqlite3 = require('lsqlite3')
+
+-- 12 Hours
+BUMP_DELAY_MS = 12 * 60 * 60 * 1000
+
+WaitlistDb = WaitlistDb or sqlite3.open_memory()
+WaitlistDbAdmin = WaitlistDbAdmin or require('DbAdmin').new(WaitlistDb)
+
+--#region Initialization
+
+SQLITE_TABLE_WAITLIST = [[
+  CREATE TABLE IF NOT EXISTS Waitlist (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    WalletId TEXT UNIQUE,
+    TimestampCreated INTEGER,
+    TimestampLastBumped INTEGER,
+    BumpCount INTEGER
+  );
+]]
+
+function WaitlistDbInit()
+  WaitlistDb:exec(SQLITE_TABLE_WAITLIST)
+end
+
+WaitlistInitialized = WaitlistInitialized or false
+if (not WaitlistInitialized) then
+  WaitlistDbInit()
+  WaitlistInitialized = true
+end
+
+--#endregion
+
+--#region Reads
+
+function ValidateOffset(testOffset)
+  if (type(testOffset) ~= "number") then
+    return false
+  end
+  if (testOffset < 0) then
+    return false
+  end
+
+  return true
+end
+
+function ValidateLimit(testLimit)
+  if (type(testLimit) ~= "number") then
+    return false
+  end
+  if (testLimit < 0) then
+    return false
+  end
+  if (testLimit > 100) then
+    return false
+  end
+
+  return true
+end
+
+function QueryRankWaitlist(offset, limit, bumpCountOrder, timestampLastBumpedOrder, ranks)
+  local query = [[
+    SELECT
+      *
+    FROM
+      Waitlist
+    ORDER BY
+      BumpCount ]] .. bumpCountOrder .. [[,
+      TimestampLastBumped ]] .. timestampLastBumpedOrder .. [[
+
+    LIMIT ? OFFSET ?;]]
+  local stmt = WaitlistDb:prepare(query)
+  stmt:bind_values(
+    limit or 20,
+    offset or 0
+  )
+
+  local rankEntries = {}
+  local index = 1
+  print(ranks)
+  for row in stmt:nrows() do
+    table.insert(rankEntries, {
+      Rank = ranks[index],
+      Id = row.Id,
+      WalletId = row.WalletId,
+      TimestampCreated = row.TimestampCreated,
+      TimestampLastBumped = row.TimestampLastBumped,
+      BumpCount = row.BumpCount
+    })
+    index = index + 1
+  end
+  stmt:finalize()
+
+  return rankEntries
+end
+
+function Range(from, to, step)
+  step = step or 1
+  local range = {}
+  for i = from, to, step do
+    table.insert(range, i)
+  end
+  return range
+end
+
+Handlers.add(
+  "WaitlistState",
+  Handlers.utils.hasMatchingTag("Read", "Waitlist-State"),
+  function(msg)
+    print("WaitlistState")
+    -- Doesn't need to be validated
+    local userAddress = msg.From
+
+    local limitTop = tonumber(msg.Tags['Limit-Top']) or 10
+    if (not ValidateLimit(limitTop)) then
+      return print("Invalid Limit-Top")
+    end
+    local limitBottom = tonumber(msg.Tags['Limit-Bottom']) or 10
+    if (not ValidateLimit(limitBottom)) then
+      return print("Invalid Limit-Bottom")
+    end
+    local limitSurroundingPlusMinus = tonumber(msg.Tags['Limit-Surrounding']) or 10
+    if (not ValidateLimit(limitSurroundingPlusMinus)) then
+      return print("Invalid Limit-Surrounding")
+    end
+
+    local waitlistCount = WaitlistDbAdmin:count('Waitlist')
+
+    local waitlistUserPosition = 0;
+    -- Get the rank of the user
+    -- Sort by BumpCount DESC, TimestampLastBumped ASC
+    -- Should be safe to string.format msg.From
+    local waitlistQuery = WaitlistDbAdmin:exec(string.format([[
+    SELECT
+      Rank
+    FROM
+      (
+        SELECT
+          WalletId,
+          ROW_NUMBER() OVER (ORDER BY BumpCount DESC, TimestampLastBumped ASC) AS Rank
+        FROM
+          Waitlist
+      )
+    WHERE
+      WalletId = '%s'
+    ]], userAddress))
+
+    if (waitlistQuery and #waitlistQuery == 1) then
+      waitlistUserPosition = waitlistQuery[1].Rank
+    end
+
+    -- Query waitlist limits
+    local rankAscEntries = QueryRankWaitlist(0, limitTop, "DESC", "ASC", Range(1, limitTop))
+    local rankDescEntries = QueryRankWaitlist(0, limitBottom, "ASC", "DESC",
+      Range(waitlistCount, waitlistCount - limitBottom, -1))
+
+    local surroundingLower = math.max(waitlistUserPosition - limitSurroundingPlusMinus, 0)
+    local surroundingUpper = math.min(waitlistUserPosition + limitSurroundingPlusMinus, waitlistCount)
+    local rankAscSurroundingEntries = QueryRankWaitlist(
+      surroundingLower,
+      limitSurroundingPlusMinus * 2 + 1,
+      "DESC",
+      "ASC",
+      Range(surroundingLower + 1, surroundingUpper + 1)
+    )
+
+    -- Reply with messages
+    Send({
+      Target = msg.From,
+      Tags = {
+        Action = "Waitlist-State-Response",
+      },
+      Data = json.encode({
+        Count = waitlistCount,
+        UserPosition = waitlistUserPosition,
+        RankDesc = rankDescEntries,
+        RankAsc = rankAscEntries,
+        RankAscSurrounding = rankAscSurroundingEntries,
+      })
+    })
+  end
+)
+
+Handlers.add(
+  "WaitlistPage",
+  Handlers.utils.hasMatchingTag("Read", "Waitlist-Page"),
+  function(msg)
+    print("WaitlistPage")
+
+    local offset = tonumber(msg.Tags['Offset']) or 0
+    if (not ValidateOffset(offset)) then
+      return print("Invalid Offset")
+    end
+
+    local limit = tonumber(msg.Tags['Limit']) or 10
+    if (not ValidateLimit(limit)) then
+      return print("Invalid Limit")
+    end
+
+    -- Query waitlist
+    local rankDescEntries = QueryRankWaitlist(offset, limit, "DESC", "ASC", Range(offset + 1, offset + 1 + limit))
+
+    -- Reply with messages
+    Send({
+      Target = msg.From,
+      Tags = {
+        Action = "Waitlist-Page-Response",
+      },
+      Data = json.encode({
+        Offset = offset,
+        Limit = limit,
+        Page = rankDescEntries,
+      })
+    })
+  end
+)
+
+--#endregion
+
+--#region Writes
+
+Handlers.add(
+  "WaitlistAdd",
+  Handlers.utils.hasMatchingTag("Action", "Waitlist-Add"),
+  function(msg)
+    print("WaitlistAdd")
+
+    local userAddress = msg.From
+    local nowMs = msg.Timestamp
+
+    local stmt = WaitlistDb:prepare([[
+      INSERT INTO Waitlist (WalletId, TimestampCreated, TimestampLastBumped, BumpCount)
+      VALUES (?, ?, ?, ?)
+    ]])
+    stmt:bind_values(
+      userAddress,
+      nowMs,
+      nowMs,
+      0
+    )
+    stmt:step()
+    local result = stmt:finalize()
+
+    if (result == 19) then
+      return print("User already in waitlist")
+    end
+
+    if (result ~= 0) then
+      return print("Failed to add user to waitlist")
+    end
+
+    -- Return the entry back to the user
+    Send({
+      Target = msg.From,
+      Tags = {
+        Action = "Waitlist-Add-Response",
+      },
+      Data = json.encode({
+        WalletId = userAddress,
+        TimestampCreated = nowMs,
+        TimestampLastBumped = nowMs,
+        BumpCount = 0
+      })
+    })
+  end
+)
+
+Handlers.add(
+  "WaitlistBump",
+  Handlers.utils.hasMatchingTag("Action", "Waitlist-Bump"),
+  function(msg)
+    print("WaitlistBump")
+
+    local userAddress = msg.From
+    local nowMs = msg.Timestamp
+
+    -- Get the current entry
+    local currentRows = WaitlistDbAdmin:exec(string.format([[
+      SELECT * FROM Waitlist WHERE WalletId = '%s'
+    ]], userAddress))
+    -- Get length of currentRow table
+    if (not currentRows or #currentRows == 0) then
+      return print("User not found in waitlist")
+    end
+
+    local currentRow = currentRows[1]
+    local lastBumpedMs = currentRow.TimestampLastBumped
+
+    -- Check if the user is allowed to bump
+    if (nowMs - lastBumpedMs < BUMP_DELAY_MS) then
+      return print("User cannot bump yet")
+    end
+
+    -- Update the entry
+    local newBumpCount = currentRow.BumpCount + 1
+    WaitlistDbAdmin:exec(string.format([[
+      UPDATE Waitlist
+      SET TimestampLastBumped = %d,
+          BumpCount = %d
+      WHERE WalletId = '%s'
+    ]], nowMs, newBumpCount, userAddress))
+
+    -- Build updated entry
+    local newEntry = {
+      WalletId = userAddress,
+      TimestampCreated = currentRow.TimestampCreated,
+      TimestampLastBumped = nowMs,
+      BumpCount = newBumpCount
+    }
+
+    -- Return the entry back to the user
+    Send({
+      Target = msg.From,
+      Tags = {
+        Action = "Waitlist-Bump-Response",
+      },
+      Data = json.encode(newEntry)
+    })
+  end
+)
+
+--#endregion
+
+return "Loaded Waitlist Protocol"
---
+++ process/misc/Waitlist.test.js
@@ -0,0 +1,255 @@
+import { test } from 'node:test'
+import * as assert from 'node:assert'
+import { Send } from '../aos.helper.js'
+import fs from 'node:fs'
+
+const TWELVE_HOURS_MS = 12 * 60 * 60 * 1000
+
+test('load DbAdmin module', async () => {
+  const dbAdminCode = fs. readFileSync('./misc/DbAdmin.lua', 'utf-8')
+  const result = await Send({
+  Action: 'Eval',
+  Data: `
+local function _load()
+  ${dbAdminCode}
+end
+_G.package.loaded["DbAdmin"] = _load()
+return "ok"`,
+  })
+  assert.equal(result.Output.data.output, "ok")
+})
+
+test('load source', async () => {
+  const code = fs.readFileSync('./misc/Waitlist.lua', 'utf-8')
+  const result = await Send({ Action: "Eval", Data: code })
+
+  assert.equal(result.Output.data.output, "Loaded Waitlist Protocol")
+})
+
+test('WaitlistState empty waitlist', async () => {
+  const result = await Send({
+    Read: "Waitlist-State",
+  })
+
+  assert.ok(result)
+});
+
+test('WaitlistPage empty waitlist', async () => {
+  const result = await Send({
+    Read: "Waitlist-Page",
+  })
+
+  const reply = result.Messages[0]
+
+  const page = JSON.parse(reply.Data)['Page']
+  assert.equal(page.length, 0)
+});
+
+test('WaitlistBump with empty waitlist', async () => {
+  const result = await Send({
+    Action: "Waitlist-Bump",
+  });
+
+  assert.equal(result.Messages.length, 0)
+  assert.ok(result.Output.data.endsWith("User not found in waitlist"))
+});
+
+test('WaitlistAdd success', async () => {
+  const result = await Send({
+    Action: "Waitlist-Add",
+  });
+
+  const reply = result.Messages[0]
+  const replyData = JSON.parse(reply.Data)
+  assert.deepEqual(replyData, {"TimestampCreated":10003,"TimestampLastBumped":10003,"WalletId":"OWNER","BumpCount":0})
+
+  const dbResult = await Send({
+    Action: 'Eval',
+    Data: "require('json').encode(WaitlistDbAdmin:exec('SELECT * FROM Waitlist'))"
+  })
+  const dbOutput = JSON.parse(dbResult.Output.data.output)
+  assert.deepEqual(dbOutput, [
+    {
+      TimestampCreated: 10003,
+      WalletId: 'OWNER',
+      Id: 1,
+      TimestampLastBumped: 10003,
+      BumpCount: 0
+    }
+  ])
+})
+
+test('WaitlistAdd retry reject', async () => {
+  const result = await Send({
+    Action: "Waitlist-Add",
+    Timestamp: 10004,
+  });
+
+  assert.equal(result.Messages.length, 0)
+  assert.ok(result.Output.data.endsWith("User already in waitlist"))
+
+  // Db should be the same
+  const dbResult = await Send({
+    Action: 'Eval',
+    Data: "require('json').encode(WaitlistDbAdmin:exec('SELECT * FROM Waitlist'))"
+  })
+  const dbOutput = JSON.parse(dbResult.Output.data.output)
+  assert.deepEqual(dbOutput, [
+    {
+      TimestampCreated: 10003,
+      WalletId: 'OWNER',
+      Id: 1,
+      TimestampLastBumped: 10003,
+      BumpCount: 0
+    }
+  ])
+})
+
+test('WaitlistBump too soon fails', async () => {
+  const result = await Send({
+    Action: "Waitlist-Bump",
+    Timestamp: 10005 + TWELVE_HOURS_MS - 100,
+  });
+
+  assert.equal(result.Messages.length, 0)
+  assert.ok(result.Output.data.endsWith("User cannot bump yet"))
+
+  // Db should be the same
+  const dbResult = await Send({
+    Action: 'Eval',
+    Data: "require('json').encode(WaitlistDbAdmin:exec('SELECT * FROM Waitlist'))"
+  })
+  const dbOutput = JSON.parse(dbResult.Output.data.output)
+  assert.deepEqual(dbOutput, [
+    {
+      TimestampCreated: 10003,
+      WalletId: 'OWNER',
+      Id: 1,
+      TimestampLastBumped: 10003,
+      BumpCount: 0
+    }
+  ])
+});
+
+test('WaitlistBump in ages success', async () => {
+  const agesTs = 10005 + TWELVE_HOURS_MS + 100
+  const result = await Send({
+    Action: "Waitlist-Bump",
+    Timestamp: agesTs,
+  });
+
+  const reply = result.Messages[0]
+  const replyData = JSON.parse(reply.Data)
+  assert.deepEqual(replyData, {
+    "WalletId": "OWNER",
+    "TimestampCreated": 10003,
+    "TimestampLastBumped": agesTs,
+    "BumpCount": 1,
+  })
+});
+
+test('WaitlistBump just after fails', async () => {
+  const agesPlusTs = 10005 + TWELVE_HOURS_MS + 100 + 100
+  const result = await Send({
+    Action: "Waitlist-Bump",
+    Timestamp: agesPlusTs,
+  });
+
+  assert.equal(result.Messages.length, 0)
+  assert.ok(result.Output.data.endsWith("User cannot bump yet"))
+});
+
+test('WaitlistAdd another success', async () => {
+  const result = await Send({
+    From: "ANOTHER",
+    Action: "Waitlist-Add",
+    Timestamp: 10006,
+  });
+
+  const reply = result.Messages[0]
+  const replyData = JSON.parse(reply.Data)
+  assert.deepEqual(replyData, {"TimestampCreated":10006,"TimestampLastBumped":10006,"WalletId":"ANOTHER","BumpCount":0})
+
+  const dbResult = await Send({
+    Action: 'Eval',
+    Data: "require('json').encode(WaitlistDbAdmin:exec('SELECT * FROM Waitlist'))"
+  })
+  const dbOutput = JSON.parse(dbResult.Output.data.output)
+  assert.deepEqual(dbOutput, [
+    {
+      TimestampCreated: 10003,
+      WalletId: 'OWNER',
+      Id: 1,
+      TimestampLastBumped: 43210105,
+      BumpCount: 1
+    },
+    {
+      TimestampCreated: 10006,
+      WalletId: 'ANOTHER',
+      Id: 2,
+      TimestampLastBumped: 10006,
+      BumpCount: 0
+    }
+  ])
+})
+
+test('Range', async () => {
+  const result = await Send({
+    Action: "Eval",
+    Data: "require('json').encode(Range(10, 5, -1))"
+  })
+
+  assert.deepEqual(JSON.parse(result.Output.data.output), [
+    10,
+    9,
+    8,
+    7,
+    6,
+    5
+  ])
+});
+
+test('WaitlistState main user', async () => {
+  const result = await Send({
+    Read: "Waitlist-State",
+  })
+  // console.log(result)
+
+  const reply = result.Messages[0]
+  const replyData = JSON.parse(reply.Data)
+  // console.log(replyData)
+  assert.equal(replyData.RankDesc.length, 2)
+  assert.equal(replyData.Count, 2);
+  assert.equal(replyData.UserPosition, 1)
+});
+
+
+test('WaitlistState second user', async () => {
+  const result = await Send({
+    From: "ANOTHER",
+    Read: "Waitlist-State",
+  })
+  // console.log(result)
+
+  const reply = result.Messages[0]
+  const replyData = JSON.parse(reply.Data)
+  // console.log(replyData)
+  assert.equal(replyData.RankDesc.length, 2)
+  assert.equal(replyData.Count, 2);
+  assert.equal(replyData.UserPosition, 2)
+});
+
+test('WaitlistState Unknown user', async () => {
+  const result = await Send({
+    From: "Who?",
+    Read: "Waitlist-State",
+  })
+  // console.log(result)
+
+  const reply = result.Messages[0]
+  const replyData = JSON.parse(reply.Data)
+  console.log(replyData)
+  assert.equal(replyData.RankDesc.length, 2)
+  assert.equal(replyData.Count, 2);
+  assert.equal(replyData.UserPosition, 0)
+});
