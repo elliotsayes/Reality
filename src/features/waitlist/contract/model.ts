85c7ecd7f5315b5a04fad6b8394e4f3e5c488eb8
Elliot Sayes <elliot@sayes.cloud>
2024-06-20 12:44:05 +0200

    WaitlistClient (untested)

---
+++ src/features/waitlist/contract/model.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const WaitlistEntry = z.object({
+  Rank: z.number(),
+  Id: z.number(),
+  TimestampCreated: z.number(),
+  TimestampLastBumped: z.number(),
+  BumpCount: z.number(),
+});
+export type WaitlistEntry = z.infer<typeof WaitlistEntry>;
+
+export const RankList = z.array(WaitlistEntry);
+export type RankList = z.infer<typeof RankList>;
+
+export const WaitlistState = z.object({
+  RankAsc: RankList,
+  RankAscSurrounding: RankList,
+  RankDesc: RankList,
+  Count: z.number(),
+  UserPosition: z.number(),
+});
+export type WaitlistState = z.infer<typeof WaitlistState>;
---
+++ src/features/waitlist/contract/waitlistClient.ts
@@ -0,0 +1,50 @@
+import { AoContractClient, createAoContractClient } from "../../ao/lib/aoContractClient";
+import { AoWallet } from "../../ao/lib/aoWallet";
+import { ArweaveTxId } from "../../arweave/lib/model";
+import { WaitlistState } from "./model";
+import { connect } from "@permaweb/aoconnect";
+
+export type WaitlistClient = {
+  aoContractClient: AoContractClient;
+
+  // Reads
+  readState(): Promise<WaitlistState>;
+
+  // Writes
+  register(): Promise<ArweaveTxId>;
+  bump(): Promise<ArweaveTxId>;
+}
+
+// Placeholder
+// TODO: Define these methods properly
+export const createWaitlistClient = (
+  aoContractClient: AoContractClient,
+): WaitlistClient => ({
+  aoContractClient: aoContractClient,
+
+  // Reads
+  readState: () => aoContractClient.dryrunReadReplyOneJson<WaitlistState>({
+    tags: [{ name: "Action", value: "Waitlist-State" }],
+  }),
+
+  // Writes
+  register: () => aoContractClient.message({
+    tags: [{
+      name: "Action",
+      value: "Waitlist-Register",
+    }],
+  }),
+  bump: () => aoContractClient.message({
+    tags: [{
+      name: "Action",
+      value: "Waitlist-Bump",
+    }],
+  }), 
+});
+
+export const createWaitlistClientForProcess = (wallet: AoWallet) => (processId: string) => {
+  const aoContractClient = createAoContractClient(processId, connect(), wallet);
+  return createWaitlistClient(aoContractClient);
+}
+
+export type WaitlistClientForProcess = ReturnType<typeof createWaitlistClientForProcess>;
