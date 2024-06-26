f822dde2b9316baeff29ef211c71e4099878b08e
Elliot Sayes <elliot@sayes.cloud>
2024-06-26 18:59:56 +0200

    Dumping waitlist

---
+++ process/misc/WaitlistDump.js
@@ -0,0 +1,17 @@
+import { dryrun } from "@permaweb/aoconnect";
+import { writeFileSync } from "fs";
+import json2csv from "json2csv";
+
+dryrun({
+  process: "2dFSGGlc5xJb0sWinAnEFHM-62tQEbhDzi1v5ldWX5k",
+  Owner: "K3FysbyRLGwzJByRZOOz1I6ybQtZI3kFNMtmkC4IkoQ",
+  tags: [{ name: "Action", value: "Eval" }],
+  data: "require('json').encode(WaitlistDbAdmin:exec('SELECT * FROM Waitlist'))",
+}).then((result) => {
+  console.log(result.Output.data.output);
+  writeFileSync("./misc/WaitlistDump.json", result.Output.data.output);
+  // convert to csv
+  // install command for json2csv: npm install json2csv
+  const csv = json2csv.parse(JSON.parse(result.Output.data.output));
+  writeFileSync("./misc/WaitlistDump.csv", csv);
+});
