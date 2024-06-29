583a5e7e1921c4f00d267510c96e8573bcf5d11c
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2024-06-29 16:13:13 -0700

    add styling updates to form components

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -8,10 +8,7 @@ const Card = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
-      className,
-    )}
+    className={cn("bg-card text-card-foreground shadow-sm", className)}
     {...props}
   />
 ));
@@ -36,7 +33,7 @@ const CardTitle = React.forwardRef<
   <h3
     ref={ref}
     className={cn(
-      "text-2xl font-semibold leading-none tracking-tight",
+      "text-xl font-semibold leading-none tracking-tight font-Press-Start-2P",
       className,
     )}
     {...props}
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
     extend: {
       fontFamily: {
         "undead-pixel": ["Undead\\ Pixel\\ 11", "sans-serif"],
+        "Press-Start-2P": ["Press\\ Start\\ 2P", "sans-serif"],
       },
       fontSize: {
         "22px": "1.375rem",
