88d1b5b065ccabf61373d01bcf783c9021d9ebfb
Elliot Sayes <elliot@sayes.cloud>
2024-07-05 12:48:18 +0200

    Create tutorial

--- src/features/render/components/TutorialOverlay.tsx
+++ src/features/render/components/TutorialOverlay.tsx
@@ -6,12 +6,18 @@ import { X } from "lucide-react";
 import { useState } from "react";
 import { toast } from "sonner";
 
+// Tailwind styles to look like key on the keyboard
+function KeyboardKey({ children }: { children: string }) {
+  return <kbd className="bg-gray-200 p-1 rounded-md shadow-md">{children}</kbd>;
+}
+
 interface FormOverlayProps {
   close: () => void;
 }
 
 export default function TutorialOverlay({ close }: FormOverlayProps) {
   const [page, setPage] = useState(0);
+  const [doneEnabled, setDoneEnabled] = useState(false);
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -22,7 +28,62 @@ export default function TutorialOverlay({ close }: FormOverlayProps) {
             <X />
           </Button>
         </CardHeader>
-        <CardContent>Tutorial Content</CardContent>
+        <CardContent className="font-undead-pixel text-lg">
+          <div>
+            {page === 0 ? (
+              // Show arrow keys for movement
+              <div className="flex flex-row justify-evenly items-end gap-4 pt-4 pb-10 h-32">
+                <div>
+                  Arrow keys
+                  <br />
+                  for movement
+                </div>
+                <div className="flex flex-col items-center leading-3">
+                  <div className="flex flex-row items-center">
+                    <KeyboardKey>↑</KeyboardKey>
+                  </div>
+                  <div className="flex flex-row items-center">
+                    <KeyboardKey>←</KeyboardKey> <KeyboardKey>↓</KeyboardKey>{" "}
+                    <KeyboardKey>→</KeyboardKey>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="flex flex-row justify-evenly items-center gap-4 pt-4 pb-10 h-32">
+                <div>
+                  Click on NPCs
+                  <br />
+                  to interact
+                </div>
+                <img src="assets/tutorial/click_king.png" width={100} />
+              </div>
+            )}
+          </div>
+          <div className="flex flex-row justify-between gap-4">
+            <Button
+              onClick={() => setPage((prev) => Math.max(0, prev - 1))}
+              variant={"outline"}
+              disabled={page === 0}
+            >
+              Prev
+            </Button>
+            {page === 0 ? (
+              <Button
+                onClick={() => {
+                  setPage((prev) => prev + 1);
+                  setTimeout(() => setDoneEnabled(true), 1000);
+                }}
+                variant={"outline"}
+              >
+                Next
+              </Button>
+            ) : (
+              <Button onClick={close} disabled={!doneEnabled}>
+                Done
+              </Button>
+            )}
+          </div>
+        </CardContent>
       </Card>
     </QueryClientProvider>
   );
