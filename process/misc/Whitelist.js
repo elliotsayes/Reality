6e79a3357b02670e986c241511e8bb90da7b70a8
Elliot Sayes <elliot@sayes.cloud>
2024-07-13 11:42:13 +0200

    whitelist

---
+++ process/misc/Whitelist.js
@@ -0,0 +1,23 @@
+import { message, createDataItemSigner } from "@permaweb/aoconnect";
+import { whitelist } from "./WhitelistDataBatch1";
+import fs from "fs";
+
+const key = JSON.parse(
+  fs.readFileSync("../.secret/deploy_sqlite_jwk.json", "utf8"),
+);
+
+async function main() {
+  const signer = createDataItemSigner(key);
+
+  for (const walletId of whitelist) {
+    const res = await message({
+      process: "2dFSGGlc5xJb0sWinAnEFHM-62tQEbhDzi1v5ldWX5k",
+      tags: [{ name: "Action", value: "Eval" }],
+      data: `AuthoriseWallet("${walletId}")`,
+      signer,
+    });
+    console.log(res);
+  }
+}
+
+main();
