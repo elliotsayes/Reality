0a7f42f7a82d34a7e4833ec8ff3f70bdee56a196
Elliot Sayes <elliot@sayes.cloud>
2024-07-15 15:48:45 -0400

    Add link to balances page

--- src/features/main/components/Main.tsx
+++ src/features/main/components/Main.tsx
@@ -59,10 +59,14 @@ export default function Main({ wallet, disconnect, verseId }: MainProps) {
     <div>
       <div className="flex flex-row gap-4 items-center fixed top-0 right-0 py-2 px-2">
         <p>
-          Wallet:{" "}
-          <span className="font-mono text-sm text-muted-foreground">
+          Wallet{" "}
+          <a
+            href={`https://www.ao.link/#/entity/${wallet.address}?tab=balances`}
+            target="_blank"
+            className="hover:underline font-mono text-sm text-muted-foreground"
+          >
             {truncateAddress(wallet.address)}
-          </span>
+          </a>
         </p>
         <ProfileButton profileInfo={current.context.profileInfo} />
         <Button onClick={disconnect} size={"sm"} variant={"secondary"}>
